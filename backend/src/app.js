9c07d0d48387d7040335f398ad93309f10fbed57
DanielSanabria09 <danielsansol@unisabana.edu.co>
2025-10-24 07:12:29 -0500

    Agregar URL del Front a CORS

--- backend/src/app.js
+++ backend/src/app.js
@@ -15,7 +15,10 @@ app.use(cors({
     'http://127.0.0.1:5500',
     'http://localhost:5500',
     'http://127.0.0.1:5501',
-    'http://localhost:5501'
+    'http://localhost:5501',
+    'http://localhost:3000',
+    'http://localhost:5173',
+    'https://wheels-final-project-frontend.onrender.com'
   ],
   credentials: true
 }));
