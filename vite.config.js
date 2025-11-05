7a93610f09040780c98a7af4fd3b6eadf3de0666
Camilo Andrés Gutiérrez Barriga <camiloguba@unisabana.edu.co>
2025-11-04 23:06:05 -0500

    Corrigiendo Vite agregando las rutas correctas para produccion

--- vite.config.js
+++ vite.config.js
@@ -18,7 +18,11 @@ export default defineConfig({
                 login: resolve(__dirname, 'frontend/pages/shared/login.html'),
                 'register-rider': resolve(__dirname, 'frontend/pages/shared/register-rider.html'),
                 'register-vehicle': resolve(__dirname, 'frontend/pages/shared/register-vehicle.html'),
-                dashboard: resolve(__dirname, 'frontend/pages/shared/dashboard.html')
+                dashboard: resolve(__dirname, 'frontend/pages/shared/dashboard.html'),
+                'profile-view': resolve(__dirname, 'frontend/pages/shared/profile-view.html'),
+                'profile-view-rider': resolve(__dirname, 'frontend/pages/shared/profile-view-rider.html'),
+                'profile-edit': resolve(__dirname, 'frontend/pages/shared/profile-edit.html'),
+                'profile-edit-rider': resolve(__dirname, 'frontend/pages/shared/profile-edit-rider.html')
             }
         },
         assetsInlineLimit: 0  // No inline assets, mantener archivos separados
